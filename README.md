02d400d48d97936264318e3b25b5421812bbc2e6
PatrickKR <mailpatrickkr@gmail.com>
2021-05-28 00:17:07 +0900

    정리!

--- README.md
+++ README.md
@@ -4,8 +4,5 @@ an attach library for bukkit-based minecraft server
 
 working in progress
 
-Motivation from:
-
- - [monun - tap (pre-3.0)](https://github.com/monun/tap)
- - [Electronic Arts - EA Agent Loader](https://github.com/electronicarts/ea-agent-loader)
- - [ASM & Attach API Tutorial from Bukkit](https://bukkit.org/threads/tutorial-extreme-beyond-reflection-asm-replacing-loaded-classes.99376/)
+Special thanks to:
+ - [dmulloy2 - ProtocolLib (Defining Class)](https://github.com/dmulloy2/ProtocolLib)
--- src/main/kotlin/com/github/patrick/attach/plugin/Main.kt
+++ src/main/kotlin/com/github/patrick/attach/plugin/Main.kt
@@ -3,19 +3,14 @@ package com.github.patrick.attach.plugin
 import com.github.patrick.attach.Attach.patch
 
 fun main() {
+    val test = Test()
+    test.run()
 
-    try {
-        val test = Test()
-        test.run()
+    Test::class.java.patch("run", prefix = {
+        println("Foo")
+    }, postfix = {
+        println("Bar")
+    })
 
-        Test::class.java.patch("run", prefix = {
-            println("Foo")
-        }, postfix = {
-            println("Bar")
-        })
-
-        test.run()
-    } catch (t: Throwable) {
-        t.printStackTrace()
-    }
+    test.run()
 }
